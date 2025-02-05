fcc5543db6baafb2262915284181418bbcd3e537
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-02-05 21:36:32 +0100

    payment table add

--- Domain/Order.cs
+++ Domain/Order.cs
@@ -9,11 +9,8 @@ public class Order
     public decimal Discount { get; set; } = 0;
 
     public OrderStatus OrderStatus { get; set; } = OrderStatus.Ongoing;
-    public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Pending;
 
     public required OrderType OrderType { get; set; }
-
-    public PaymentMethod? PaymentMethod { get; set; }
 
     public List<OrderItem> OrderItems { get; set; } = new();
 
@@ -31,23 +28,9 @@ public enum OrderStatus
     Cancelled
 }
 
-public enum PaymentStatus
-{
-    Pending,
-    Paid,
-    DefferedPayment,
-    Cancelled
-}
-
 public enum OrderType
 {
     DineIn,
     Takeaway,
     Delivery
-}
-
-public enum PaymentMethod
-{
-    Cash,
-    Card
 }
---
+++ Domain/Payment.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain;
+
+public class Payment
+{
+    public Guid Id { get; set; }
+    public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Pending;
+    public PaymentMethod? PaymentMethod { get; set; }
+
+}
+
+public enum PaymentStatus
+{
+    Pending,
+    Paid,
+    DefferedPayment,
+    Cancelled
+}
+
+public enum PaymentMethod
+{
+    Cash,
+    Card
+}
