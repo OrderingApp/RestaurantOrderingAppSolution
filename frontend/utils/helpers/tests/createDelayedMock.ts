907e4bee3ae800d9f0b2b91f1ebba6fe796e9928
Patryk Sąsiadek <patrykskontakt@gmail.com>
2025-02-10 19:56:44 +0100

    created a dir for helpers/tests due to tsdocs etc and imported & exported it from overall test-helpers.ts

---
+++ frontend/utils/helpers/tests/createDelayedMock.ts
@@ -0,0 +1,32 @@
+import { vi } from 'vitest';
+
+/**
+ * Creates a mock function that resolves after a specified delay.
+ *
+ * This function is useful for simulating asynchronous behavior in tests, such as API calls or other delayed operations.
+ * The mock function resolves with `undefined` after the specified delay.
+ *
+ * @param delay - The delay in seconds before the mock function resolves.
+ * @returns A mock function that resolves with `undefined` after the specified delay.
+ *
+ * @example
+ *
+ * it('should handle delayed action', async () => {
+ *   const handleClick = createDelayedMock(1);
+ *   render(<Button action={handleClick}>Click Me</Button>);
+ *
+ *   await userEvent.click(screen.getByRole('button', { name: 'Click Me' }));
+ *   await waitFor(() => expect(handleClick).toHaveBeenCalledOnce());
+ * });
+ */
+const createDelayedMock = (delay: number) =>
+    vi
+        .fn()
+        .mockImplementation(
+            () =>
+                new Promise<void>((res) =>
+                    setTimeout(() => res(undefined), delay * 1000)
+                )
+        );
+
+export default createDelayedMock;
