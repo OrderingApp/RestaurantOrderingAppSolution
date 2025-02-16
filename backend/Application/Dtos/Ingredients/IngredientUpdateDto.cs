cf21d4c6f2ae44f2f0ab3a8bbb4642fcff4b1357
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-02-16 12:21:43 +0100

    Updated Ingredient. Resolved #21
    added new endpoint
    - AddTagsToIngredient
    also enchanced dtos and added new validators, updated mapping.

--- backend/Application/Dtos/Ingredients/IngredientUpdateDto.cs
+++ backend/Application/Dtos/Ingredients/IngredientUpdateDto.cs
@@ -3,7 +3,6 @@
 public class IngredientUpdateDto
 {
     public string? Name { get; set; }
-    public decimal Price { get; set; }
-    public string? IngredientType { get; set; }
+    public decimal? Price { get; set; }
     public bool? IsUsed { get; set; }
 }
