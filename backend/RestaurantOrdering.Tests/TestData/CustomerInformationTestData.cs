2cbe5c6c4a3e03c37fc07f598b49dfb05999db8c
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-07-13 09:51:13 +0200

    Customer Information test update with const

---
+++ backend/RestaurantOrdering.Tests/TestData/CustomerInformationTestData.cs
@@ -0,0 +1,35 @@
+﻿using Application.Dtos.CustomerInformations;
+using Domain;
+
+namespace RestaurantOrdering.Tests.TestData;
+
+public static class CustomerInformationTestData
+{
+    public const string DefaultPhone = "123456789";
+    public const string UpdatedPhone = "987654321";
+
+    public static CustomerInformation CreateValidCustomer(Guid? id = null)
+    {
+        return new CustomerInformation
+        {
+            Id = id ?? Guid.NewGuid(),
+            PhoneNumber = DefaultPhone,
+            OrderCompletionType = OrderCompletionType.Immediate,
+            PreferredPaymentMethod = PreferredPaymentMethod.Cash,
+            OrderId = Guid.NewGuid(),
+            Order = new Order()
+        };
+    }
+
+    public static CustomerInformationUpdateDto CreateUpdateDto() => new()
+    {
+        PhoneNumber = UpdatedPhone,
+        OrderCompletionType = OrderCompletionType.Scheduled,
+        PreferedPaymentMethod = PreferredPaymentMethod.Card
+    };
+
+    public static CustomerInformationReadDto CreateReadDto(Guid id) => new()
+    {
+        PhoneNumber = UpdatedPhone
+    };
+}
