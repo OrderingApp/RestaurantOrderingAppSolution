6c96ec9839318b441908902fabdea5c72796f79e
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-07-27 16:13:09 +0200

    tag unit tests, end of unit testing branch

--- backend/RestaurantOrdering.Tests/TestData/TagTestData.cs
+++ backend/RestaurantOrdering.Tests/TestData/TagTestData.cs
@@ -8,5 +8,27 @@ public static class TagTestData
     public const string SpicyTag = "Spicy";
     public const string MeatTag = "Meat";
 
+
+    public const string DefaultTagName = "Spicy";
+    public const string UpdatedTagName = "Mild";
+
     public static Tag CreateTag(string name) => new() { Id = Guid.NewGuid(), Name = name };
+
+    public static Tag CreateValidTag(Guid? id = null, string? name = null, bool isUsed = true, bool isDeleted = false)
+    {
+        return new Tag
+        {
+            Id = id ?? Guid.NewGuid(),
+            Name = name ?? DefaultTagName,
+            IsUsed = isUsed,
+            IsDeleted = isDeleted,
+            IngredientTagRels = new()
+        };
+    }
+
+    public static List<Tag> CreateDefaultTags() => new()
+    {
+        CreateValidTag(name: "Vegan"),
+        CreateValidTag(name: "Gluten-Free")
+    };
 }
