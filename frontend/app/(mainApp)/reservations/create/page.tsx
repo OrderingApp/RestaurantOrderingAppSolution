16cf88874f07d3e3d104128e291f23fd9c27b429
KonradDepowski <depowskikonrad@gmail.com>
2025-12-13 20:22:18 +0100

    remove unused components

--- frontend/app/(mainApp)/reservations/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import CreateReservation from '@/components/pages/createReservation/CreateReservation';
-
-const CreateReservations = () => <CreateReservation />;
-
-export default CreateReservations;
